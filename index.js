2e466252f35aded1471913ae7950d9de830a46d4
Vinicius Kowalski <vinicius.kowalski@animati.com.br>
2025-03-12 21:16:07 -0300

    Comandos git básicos

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 /*Comandos git*/
+// git init                     para inicia o git
+// git add .                    para incluir todos os arquivos a ser enviado para o github
+// git commit -m  "mensagem"    para fazer o commit junto se coloca msg da alteração feita 
+// git branch -M main para mudar a branch de master para main
+// git remote add origin (link repositori) para informar ao git, para onde no github será enviado
+// git push -u origin  p        para enviar o(s) arquivos para o github

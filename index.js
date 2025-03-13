0d4f2218cfaaa1f3d9f4e0808381594fbb836f5b
Vinicius Kowalski <vinicius.kowalski@animati.com.br>
2025-03-12 21:22:12 -0300

    Limpa a sujeira

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 // git commit -m  "mensagem"    para fazer o commit junto se coloca msg da alteração feita 
 // git branch -M main para mudar a branch de master para main
 // git remote add origin (link repositori) para informar ao git, para onde no github será enviado
-// git push -u origin  p        para enviar o(s) arquivos para o github
+// git push -u origin           para enviar o(s) arquivos para o github
